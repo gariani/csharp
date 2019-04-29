e4423178d2f778735cf3ced41fe88b8011482c25
Daniel Garini Rafael <danielgrafael@gmail.com>
2019-04-29 20:58:39 -0300

    GraphQL project

---
+++ graphql/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace graphql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
